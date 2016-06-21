f681d4e5c831a8811e4815135170929d0d074863
Lahari Mohammed <lazhari.mohammed@outlook.com>
2016-06-21 23:41:21 +0000

    Create End point for items list

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,4 +9,10 @@ app.get('/', (req, res) => {
     res.render('./../app/index.ejs');
 });
 app.use(express.static(__dirname + '/../tmp'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.listen(7777);
+require('./routes/items')(app);
