b56ac7a60f7c0a74c49a1daaf85b84965d6cf56a
Lahari Mohammed <lazhari.mohammed@outlook.com>
2016-06-22 00:54:58 +0000

    Delete and patch item end points

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,12 +1,12 @@
 'use strict';
 const GroceryItem = require('../models/GroceryItem');
 
-module.exports = function (app) {
+module.exports = function(app) {
     app.route('/api/items')
         .get((req, res) => {
             GroceryItem.find({})
                 .exec((err, items) => {
-                    if(err) {
+                    if (err) {
                         return res.send([]);
                     }
                     return res.send(items);
@@ -15,10 +15,30 @@ module.exports = function (app) {
         .post((req, res) => {
             let item = req.body;
             new GroceryItem(item).save((err) => {
-                if(err) {
-                    return res.status(500).send({message: err.message});
+                if (err) {
+                    return res.status(500).send({
+                        message: err.message
+                    });
                 }
                 return res.status(300).send();
+            });
+        });
+    app.route('/api/items/:id')
+        .delete((req, res) => {
+            GroceryItem.find({
+                _id: req.params.id
+            }).remove();
+            return res.status(200);
+        })
+        .patch((req, res) => {
+            GroceryItem.findOne({
+                _id: req.body._id
+            }, (err, item) => {
+                for(let key in req.body) {
+                    item[key] = req.body[key];
+                }
+                item.save();
+                return res.status(200);
             });
         });
 };
